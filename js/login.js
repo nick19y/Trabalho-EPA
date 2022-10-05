b3bf9e1e33dfcdc1cf5d2d138d96961e806072cb
nick19y <godoi.nic.nic@gmail.com>
2022-10-05 02:45:18 -0300

    desenvolvimento da página de compra

--- js/login.js
+++ js/login.js
@@ -1,18 +1,13 @@
 const btnEsqueceuSenha = document.querySelector("p");
-// const btnEntrar = document.getElementById("button1");
-// const btnCriarConta = document.querySelector(".botao2");
-
-// function abreIndex(){
-//     var index = window.open("http://www.gmail.com", "_self");
-//     return index;
-// }
+const btnEntrar = document.getElementById("button1");
+const btnCriarConta = document.getElementById("button2");
 
 btnEsqueceuSenha.addEventListener("click", function(){
     window.open("esqueceu_senha.html", "_self");
 });
-// btnEntrar.addEventListener("click", function(){
-//     window.open(".html", "_self");
-// });
-// btnCriarConta.addEventListener("click", function(){
-//     window.open("cadastro.html", "_self");
-// });
+btnEntrar.addEventListener("click", function(){
+    window.open("index.html", "_self");
+});
+btnCriarConta.addEventListener("click", function(){
+    window.open("form.html", "_self");
+});
